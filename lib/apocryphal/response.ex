038876c0e67428a846f4b6b5ce55fa13e9ea4590
Cory O'Daniel <cory@coryodaniel.com>
2016-10-06 11:22:29 -0700

    Added Apocryphal.Response struct

---
+++ lib/apocryphal/response.ex
@@ -0,0 +1,20 @@
+defmodule Apocryphal.Response do
+  defstruct body: nil, headers: [], status_code: nil
+
+  def build(%HTTPoison.Response{body: body, headers: headers, status_code: status_code}) do
+    mime = Apocryphal.Transaction.extract_mime_from_content_type(headers)
+
+    %Apocryphal.Response{
+      headers: headers,
+      status_code: status_code,
+      body: parse_body(body, mime)
+    }
+  end
+
+  defp parse_body(body, mime) do
+    case body do
+      nil -> %{}
+      body -> Apocryphal.deserialize(body, mime)
+    end
+  end
+end
